e61b0d11b6aab45ff1f25b1e269317f258ab914d
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2024-01-27 10:29:20 -0600

    MainMenu, Clean Script

--- GameJam2024-unity/Assets/Scripts/MainMenu.cs
+++ GameJam2024-unity/Assets/Scripts/MainMenu.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoadSceneByIndex(int index)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(index);
