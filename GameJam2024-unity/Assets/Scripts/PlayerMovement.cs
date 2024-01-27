889b8ca80e8284f52ad01f1c971aa31fd079a4ac
ArchDawn725 <gunnells275@gmail.com>
2024-01-26 21:23:30 -0600

    V0.02
    Kid wanted it

--- GameJam2024-unity/Assets/Scripts/PlayerMovement.cs
+++ GameJam2024-unity/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     //components
     private Rigidbody2D rigidbody2D;
+    private CapsuleCollider2D capsuleCollider;
+    private BoxCollider2D boxCollider;
 
     private Vector2 moveInput;
 
@@ -17,17 +19,28 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
     private void Update()
     {
         if (isDead) { return; }
         Movement();
+        FlipSprite();
     }
     private void OnMove(InputValue value)
     {
         if (isDead) { return; }
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+    private void OnJump(InputValue value)
+    {
+        if (isDead) { return; }
+        if (!boxCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if (value.isPressed)
+        {
+            rigidbody2D.velocity += new Vector2(0, jump_Speed);
+        }
     }
     private void Movement()
     {
@@ -35,5 +48,13 @@ public class PlayerMovement : MonoBehaviour
         rigidbody2D.velocity = playerVelocity;
 
         //animator
+    }
+    private void FlipSprite()
+    {
+        if (PlayerMoving()) { transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f); }
+    }
+    private bool PlayerMoving()
+    {
+        return Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
     }
 }
