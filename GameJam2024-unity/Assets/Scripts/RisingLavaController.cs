82b53845c042815953c14a20aea73d789087245a
ArchDawn725 <gunnells275@gmail.com>
2024-01-28 12:28:43 -0600

    V0.7.1

--- GameJam2024-unity/Assets/Scripts/RisingLavaController.cs
+++ GameJam2024-unity/Assets/Scripts/RisingLavaController.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using UnityEngine;
 public class RisingLavaController : MonoBehaviour
 {
+    [SerializeField] private float StartDelay = 1.5f;
     [SerializeField] private float delay;
     [SerializeField] private float riseAmount;
     private void Start()
+    {
+        Invoke("Delay", StartDelay);
+    }
+    private void Delay()
     {
         StartCoroutine(RisingLava());
     }
